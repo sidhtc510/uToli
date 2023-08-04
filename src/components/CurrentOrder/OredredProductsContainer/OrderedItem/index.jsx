efd97bfe55a5e0f4ea6c961c885af986f9f2e0ed
sidhtc510 <sidhtc510@gmail.com>
2023-08-04 12:24:16 +0200

    .

--- src/components/CurrentOrder/OredredProductsContainer/OrderedItem/index.jsx
+++ src/components/CurrentOrder/OredredProductsContainer/OrderedItem/index.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./s.module.css";
 import Button from "../../../UI/Button";
-import { BsTrash } from "react-icons/bs";
+import { BsTrash, BsCheckLg } from "react-icons/bs";
 import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai";
 import { decrAction, deleteAction, incrAction } from "../../../../store/slices/currentOrderSlice";
 import { useDispatch } from "react-redux";
 
 export default function OrderedItem({ id, name, dsc, img, price, count }) {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const [confirmDeleting, setConfirmDeleting] = useState(false);
+
+    const deleteItem = () => {
+        if (!confirmDeleting) {
+            setConfirmDeleting(true);
+            setTimeout(() => {
+                setConfirmDeleting(false);
+            }, 3000);
+        } else {
+            dispatch(deleteAction(id));
+            setConfirmDeleting(false);
+        }
+    };
+
     return (
         <div className={s.orderedItem_item_wrap}>
             <img src={img} alt="" />
@@ -16,15 +30,13 @@ export default function OrderedItem({ id, name, dsc, img, price, count }) {
                     {count} X {name} ({price}) = {(price * count).toFixed(2)}
                 </p>
                 <div>
-                    <Button onClick={()=>dispatch(decrAction(id))}>
+                    <Button onClick={() => dispatch(decrAction(id))}>
                         <AiOutlineMinusCircle />
                     </Button>
-                    <Button onClick={()=>dispatch(incrAction(id))}>
+                    <Button onClick={() => dispatch(incrAction(id))}>
                         <AiOutlinePlusCircle />
                     </Button>
-                    <Button onClick={()=>dispatch(deleteAction(id))}>
-                        <BsTrash />
-                    </Button>
+                    <Button onClick={deleteItem}>{confirmDeleting ? <BsCheckLg /> : <BsTrash />}</Button>
                 </div>
             </div>
         </div>
