f9a1c20bdabc0e60a954d6b52495fb178f95f091
sidhtc510 <sidhtc510@gmail.com>
2023-08-11 23:17:39 +0200

    .

--- src/components/Pages/MainPage/index.jsx
+++ src/components/Pages/MainPage/index.jsx
@@ -7,15 +7,14 @@ import DatePicker from "../../DatePicker";
 
 export default function MainPage() {
     const orderHistory = useSelector(({ orderHistory }) => orderHistory);
-    
 
     return (
         <div className={orderHistory.list.length <= 0 ? s.wrapStyle : s.mainPage_wrapper}>
-            <DatePicker />
             {orderHistory.list.length <= 0 ? (
                 <img style={{ width: "300px" }} src={img} alt="" />
             ) : (
                 <>
+                    <DatePicker />
                     <OrderHistoryContainer orderHistory={orderHistory} />
                 </>
             )}
