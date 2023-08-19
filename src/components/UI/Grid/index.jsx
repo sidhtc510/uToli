e43413da23024b18c793a44dc30ae4d3cfc01700
sidhtc510 <sidhtc510@gmail.com>
2023-08-20 00:03:52 +0200

    .

--- src/components/UI/Grid/index.jsx
+++ src/components/UI/Grid/index.jsx
@@ -2,6 +2,5 @@ import React from "react";
 import s from "./s.module.css";
 export default function Grid({ children, className, ...props }) {
     props.className = [s.wrapper, className].join(" ");
-    console.log(    props.className);
     return <div {...props}>{children}</div>;
 }
